e48d2ae1bcf552dd019ef6ca8eef14d00335febb
oppofind <836575280@qq.com>
2017-12-13 22:23:13 +0800

    添加解析源代码获取类名的功能

--- Common-util/src/main/java/com/boco/common/util/ClassUtil.java
+++ Common-util/src/main/java/com/boco/common/util/ClassUtil.java
@@ -2,16 +2,20 @@ package com.boco.common.util;
 
 import com.boco.common.filter.FileNameFilter;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author sunyu
  */
 public class ClassUtil {
 	/**
-	 * 根据包名的到该报下的类
+	 * 根据包名的到该包下的类
 	 * @param modelPackage String
 	 * @param root String
 	 * @return List
@@ -35,5 +39,35 @@ public class ClassUtil {
 			e.printStackTrace();
 		}
 		return list;
+	}
+
+	/**
+	 *  读取源代码获取类名
+	 * @param filePath java源文件路径
+	 * @param simpleClassName 和简单的类名进行对比，避免再往下读取
+	 * @return
+	 */
+	public static String getClassName(String filePath,String simpleClassName) {
+		final String  regex = "(class (.*?)\\{)|(interfase (.*?)\\{)";
+		String curLine;
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(filePath));
+			while ((curLine = reader.readLine()) != null) {
+				Matcher matcher = Pattern.compile(regex, Pattern.DOTALL | Pattern.MULTILINE).matcher(curLine);
+				if (matcher.find()) {
+					String selector = matcher.group();
+					String[] strs = selector.split(" ");
+					for(String str:strs){
+						if(str.contains(simpleClassName)){
+							return str;
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
